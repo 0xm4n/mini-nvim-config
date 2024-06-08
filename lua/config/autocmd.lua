637c3bf1a57cab4accfcd75a0fb601f62619759a
0xm4n <37541366+0xm4n@users.noreply.github.com>
2024-06-07 17:25:13 -0700

    Update autocmd.lua

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -27,3 +27,22 @@ augroup qf
 augroup END
 ]]
 
+-- Vista
+vim.cmd [[
+let g:vista_default_executive = 'nvim_lsp'
+let g:vista_sidebar_width = 50
+let g:vista_echo_cursor = 0
+let g:vista_disable_statusline = 1
+let g:vista#renderer#enable_icon = 0
+let g:vista_blink = [0, 0]
+]]
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		local is_only_one_win = vim.fn.winnr("$") == 1
+		local is_vista_open = vim.fn["vista#sidebar#IsOpen"]() == 1
+
+		if is_only_one_win and is_vista_open then
+			vim.cmd("silent! q!") end end,
+})
+
